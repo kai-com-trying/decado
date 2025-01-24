e751c9dec7b4fa9b20b649d74bb2fb6e8ba63d1e
kaicomtrying <172279975+kai-com-trying@users.noreply.github.com>
2025-01-24 11:59:34 +0800

    added async function for search bar

--- src/features/searchSlice.js
+++ src/features/searchSlice.js
@@ -1,9 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const apiKey = process.env.REACT_APP_POLYGON_API_KEY;
 
 const initialState = {
   searchQuery: '',
   searchResults: [],
+  loading: false,
+  error: false,
 };
+
+export const fetchSearchResults = createAsyncThunk(
+    'search/fetchSearchResults',
+    async (searchQuery, { rejectWithValue }) => {
+        try {
+        const response = await axios.get(`https://api.polygon.io/v2/reference/tickers?sort=ticker&perpage=5&page=1&search=${searchQuery}&apiKey=${apiKey}`);
+        return response.data.results;
+        } catch (error) {
+        return rejectWithValue(error.response.data);
+        }
+    }
+);
 
 const searchSlice = createSlice({
   name: 'search',
@@ -12,8 +29,20 @@ const searchSlice = createSlice({
     setSearchQuery: (state, action) => {
       state.searchQuery = action.payload;
     },
-    setSearchResults: (state, action) => {
+  },
+  extraReducers: {
+    [fetchSearchResults.pending]: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    [fetchSearchResults.fulfilled]: (state, action) => {
+      state.loading = false;
       state.searchResults = action.payload;
+      state.error = false;
+    },
+    [fetchSearchResults.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = true;
     },
   }
 });
