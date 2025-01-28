1ab0f610af62c89ce636173a95c219b9da28031f
kaicomtrying <172279975+kai-com-trying@users.noreply.github.com>
2025-01-28 12:48:59 +0800

    removed some svgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useEffect } from 'react'
 import { RouterProvider, createBrowserRouter, Route, createRoutesFromElements } from 'react-router-dom'
