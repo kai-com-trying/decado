5510397a3a599e9b132e2d87aef7da64321910dc
kaicomtrying <172279975+kai-com-trying@users.noreply.github.com>
2025-02-18 12:15:43 +0800

    trimmed the warning messages

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -13,10 +13,7 @@ const HomePage = () => {
         <p>Just a heads up: </p>
         <br></br>
         <ul>
-          <li>⚠️Some companies are not 10 years or older, pages may not load</li>
-          <li>⚠️There are certain flaws in calculations for edge cases for now</li>
           <li>⚠️If you face an error, you may have reached maximum usage, try again the next day after 12pm</li>
-          <li>⚠️The search bar is still in development, thank you for your patience</li>
         </ul>
 
         <Link to="/stocks" >
